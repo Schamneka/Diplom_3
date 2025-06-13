9d79f8ac3599f5c068e3e466c2be612e9a962074
Kirill Shamne <kirill.shamneka@yandex.ru>
2025-06-13 18:01:23 +0500

    add ApiEndpoint and User

---
+++ src/main/java/constans/ApiEndpoint.java
@@ -0,0 +1,9 @@
+package constans;
+
+public class ApiEndpoint {
+
+    public static final String BASE_URI = "https://stellarburgers.nomoreparties.site/";
+    public static final String REGISTER_PATH = "/api/auth/register";
+    public static final String LOGIN_PATH = "/api/auth/login";
+    public static final String DELETE_PATH = "/api/auth/user";
+}
